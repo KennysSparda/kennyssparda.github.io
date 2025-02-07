4a18b621fb9c5ce4a54866760caa0b978c1ce7df
KennyVargas <kennykysv@gmail.com>
2025-02-07 11:52:05 -0300

    Alteradas as mensagens

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let messageIndex = 0
-const messages = ["Are you sure?", "Really sure?", "Are you positive?", "Pookie please...", "Just think about it!", "If you say no, I will be really sad...", "I will be very sad...", "I will be very very very sad..."]
+//const messages = ["Are you sure?", "Really sure?", "Are you positive?", "Pookie please...", "Just think about it!", "If you say no, I will be really sad...", "I will be very sad...", "I will be very very very sad..."]
+const messages = ["Você tem certeza?", "Certeza mesmo?", "Você tem certeza?", "Xuxu, por favor...", "Apenas pense nisso!", "Se você disser não, ficarei muito triste...", "Ficarei muito triste...", "Ficarei muito, muito, muito triste..."]
 
 function handleNoClick() {
   const noButton = document.querySelector(".no-button")
